23f84268452e9f9f52ca8ca7be8cf6b72115320e
Philip Zak <zakphi@gmail.com>
2017-08-01 13:52:17 -0400

    setup auth in my app.js, made git not deal with my super duper screty key

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const logger = require('morgan')
 const path = require('path')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const cookiePArser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
 
 const app = express()
+require('dotenv').config()
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(methodOverride('method'))
+app.use(cookiePArser())
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(express.static('public'))
 
