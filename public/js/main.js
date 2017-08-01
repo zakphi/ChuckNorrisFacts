86e4e6cc86ce195d0082c6a5eff9009b872b32cf
Philip Zak <zakphi@gmail.com>
2017-08-01 12:16:32 -0400

    added jquery, can get a random fact and display it on the page

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+$(() => {
+  console.log('script loaded')
+
+  const fact = {}
+
+  $('#generate_fact').click(() => {
+    console.log('generated random fact')
+    $.ajax({
+      url: 'https://api.chucknorris.io/jokes/random',
+      method: 'GET',
+      success: (data) => {
+        // console.log(data)
+        $('section p').text(fact.fact = data.value)
+      }
+    })
+  })
+  
+})
