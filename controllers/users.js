2b06b86815a22d8c457a506ec39ff3e8fb0427a7
Philip Zak <zakphi@gmail.com>
2017-08-01 14:17:57 -0400

    created users controller

---
+++ controllers/users.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+
+const usersController = {}
+
+usersController.create() = (req, res) => {
+  const salt = bcrypt.genSaltSync()
+  const hash = bcrypt.hashSync(req.body.password, salt)
+
+  User.create({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    username: req.body.username,
+    password_digest: hash
+  })
+    .then(user => {
+      req.login(user, (err) => {
+        if(err) return next(err)
+        res.redirect('/user')
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: err})
+    })
+}
+
+module.exports = usersController
