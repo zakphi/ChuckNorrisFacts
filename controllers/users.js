0364ee90350639aa4e3b66a763a7d689b853745d
Philip Zak <zakphi@gmail.com>
2017-08-01 15:48:15 -0400

    misspelled users model file, accidentally added unnecessary parentheses

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,9 @@
 const bcrypt = require('bcryptjs')
-const User = require('../models/user')
+const User = require('../models/users')
 
 const usersController = {}
 
-usersController.create() = (req, res) => {
+usersController.create = (req, res) => {
   const salt = bcrypt.genSaltSync()
   const hash = bcrypt.hashSync(req.body.password, salt)
 
