d66cb2246939fbecab03b7149d38d8ab6c78713e
Philip Zak <zakphi@gmail.com>
2017-08-01 14:33:43 -0400

    created my auth routes

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const authRouter = express.Router()
+const passport = require('../services/auth/local')
+const authHelper = require('../services/auth/auth-helper')
+const usersController = require('../controllers/users')
+
+authRouter.get('/login', authHelper.loginRedirect, (req, res) => {
+  res.render('auth/login', {
+    currentPage: 'login'
+  })
+})
+
+authRouter.get('/register', authHelper.loginRedirect, (req, res) => {
+  res.render('auth/register', {
+    currentPage: 'register'
+  })
+})
+
+authRouter.post('/register', usersController.create)
+
+authRouter.post('/login', passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/auth/login',
+    failureFlash: true
+  })
+)
+
+authRouter.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
+module.exports = authRouter
