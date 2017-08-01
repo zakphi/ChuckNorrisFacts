3a9fcab23467997ab5a8b6528cbc665be0d72954
Philip Zak <zakphi@gmail.com>
2017-08-01 14:22:21 -0400

    created users model

---
+++ models/users.js
@@ -0,0 +1,19 @@
+const db = require('../db/config')
+
+const User = {}
+
+User.findByUsername = username => {
+  return db.oneOrNone(`
+    SELECT * FROM users
+    WHERE username = $1
+  `, [username])
+}
+
+User.create = user => {
+  return db.one(`
+    INSERT INTO users
+    (firstname, lastname, email, username, password_digest)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *
+  `, [user.firstname, user.lastname, user.email, user.username, user.password_digest])
+}
